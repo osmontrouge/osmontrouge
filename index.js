c8bcaa810f65549f94e7eaace464755b13aeb838
François de Metz <francois@2metz.fr>
2019-03-03 16:22:23 +0100

    Extract css.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import 'ol/ol.css';
 import 'ol-popup/src/ol-popup.css';
+import 'normalize.css/normalize.css';
+
 import Map from 'ol/Map';
 import View from 'ol/View';
 import { fromLonLat, transformExtent } from 'ol/proj';
