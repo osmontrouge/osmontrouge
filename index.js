7854381e603b8ab7a700f9449768ea080a636984
François de Metz <francois@2metz.fr>
2019-03-17 22:31:41 +0100

    Rewrite with vuejs.

--- index.js
+++ index.js
@@ -1,132 +1,39 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import 'normalize.css/normalize.css';
-import './index.css';
+import 'vuetify/dist/vuetify.min.css';
+import './font/osm.css';
 
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
 import data from './data.yml';
-import ejs from 'ejs';
-import geojsondata from './data/*.geojson'
+import App from './js/app.vue';
 
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'https://tiles.osmontrouge.fr/styles/liberty/style.json',
-  center: [2.3160600, 48.8144000],
-  zoom: 15,
-  maxBounds: [[2.300305, 48.809181], [2.333694, 48.822321]],
-  hash: true
+import imageSidebar from './images/montrouge.png';
+
+Vue.use(Vuetify, {
+  icons: {
+    prev: 'osm-chevron_left',
+    next: 'osm-chevron_right',
+    expand: 'osm-arrow_down',
+    checkboxOn: 'osm-check_box',
+    checkboxOff: 'osm-check_box_outline_blank'
+  }
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+const mapMaxBounds = [[2.293611, 48.807344], [2.336998, 48.825486]];
+const mapCenter = [2.315111, 48.816614];
+const mapZoom = 15;
+const mapName = 'OSMontrouge';
 
-class Sidebar {
-  onAdd(map) {
-    this._map = map;
-    this._container = document.createElement('div');
-    this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
-    this._button = document.createElement('button');
-    this._button.innerHTML = '<';
-    this._container.appendChild(this._button);
-    this._button.addEventListener('click', this._onClick.bind(this), false);
-    return this._container;
-  }
-
-  onRemove() {
-    this._container.parentNode.removeChild(this._container);
-    this._map = undefined;
-  }
-
-  _onClick() {
-    document.body.classList.toggle('sidebar-closed');
-    if (document.body.classList.contains('sidebar-closed')) {
-      this._button.innerHTML = '>';
-    } else {
-      this._button.innerHTML = '<';
+new Vue({
+  render: createEle => createEle(App, {
+    props: {
+      taxonomy: data,
+      mapMaxBounds,
+      mapCenter,
+      mapZoom,
+      mapName,
+      imageSidebar
     }
-  }
-}
-
-map.addControl(new Sidebar(), 'top-left');
-
-const filters = Object.keys(data).map((id) => {
-  return {
-    id,
-    ...data[id]
-  };
-});
-const content = ejs.render(`
-  <% filters.forEach(({ id, name }) => { %>
-    <label><input type="checkbox" data-name="<%= id %>" /> <%= name %></label>
-  <% }) %>
-`, { filters });
-document.getElementById('filters').innerHTML = content;
-
-document.querySelectorAll('input[type=checkbox]').forEach((checkbox) => {
-  let layer = null;
-  const name = checkbox.dataset.name;
-  checkbox.addEventListener('change', (e) => {
-    if (e.target.checked) {
-      map.addSource(name, {
-        type: 'geojson',
-        data: geojsondata[name]
-      });
-      const color = (data[name].style || { color: 'blue' }).color;
-      map.addLayer({
-        id: `${name}-polygon`,
-        type: 'fill',
-        source: name,
-        paint: {
-          'fill-color': color,
-          'fill-opacity': .4
-        },
-        filter: ['==', '$type', 'Polygon']
-      });
-      map.addLayer({
-        id: `${name}-point`,
-        type: 'circle',
-        source: name,
-        paint: {
-          'circle-radius': 10,
-          'circle-color': color
-        },
-        filter: ['==', '$type', 'Point']
-      });
-      bindTooltip(`${name}-polygon`);
-      bindTooltip(`${name}-point`);
-    } else {
-      map.removeLayer(`${name}-polygon`);
-      map.removeLayer(`${name}-point`);
-      map.removeSource(name);
-      unbindTooltip(`${name}-polygon`);
-      unbindTooltip(`${name}-point`);
-    }
-  });
-});
-
-const popup = new mapboxgl.Popup({
-  closeButton: false,
-  closeOnClick: false
-});
-
-function showTooltip(e) {
-  const name = e.features[0].properties.name;
-  if (!name) return;
-  map.getCanvas().style.cursor = 'pointer';
-
-  popup.setLngLat(e.lngLat)
-    .setHTML(name)
-    .addTo(map);
-}
-
-function hideTooltip() {
-  map.getCanvas().style.cursor = '';
-  popup.remove();
-}
-
-function bindTooltip(layer) {
-  map.on('mouseenter', layer, showTooltip);
-  map.on('mouseleave', layer, hideTooltip);
-}
-function unbindTooltip(layer) {
-  map.off('mouseenter', layer, showTooltip);
-  map.off('mouseleave', layer, hideTooltip);
-}
+  }),
+}).$mount('#app');
---
+++ js/sidebar.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card>
+    <v-img
+      :src="imageSidebar"
+      height="200px"
+    >
+      <v-layout
+        column
+        fill-height
+      >
+        <v-spacer></v-spacer>
+
+        <v-card-title class="white--text pl-1 pt-5">
+          <div class="display-1 pt-5">{{ mapName }}</div>
+        </v-card-title>
+      </v-layout>
+    </v-img>
+
+    <v-list>
+
+      <v-list-group
+        v-for="(category, id) in taxonomy"
+        :key="id"
+        :prepend-icon="category.icon"
+      >
+        <template v-slot:activator>
+          <v-list-tile>
+            <v-list-tile-title>{{ category.name }}</v-list-tile-title>
+          </v-list-tile>
+        </template>
+
+        <v-list-tile
+          v-for="(feature, idFeature) in category.features"
+          :key="idFeature"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title>{{ feature.name }}</v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-checkbox @change="$emit('input', id, idFeature, $event)"></v-checkbox>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list-group>
+
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    taxonomy: {
+      type: Object,
+      required: true
+    },
+    mapName: {
+      type: String,
+      required: true
+    },
+    imageSidebar: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
