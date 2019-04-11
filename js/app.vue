0dc53d429208d77d9602e65f7420cfcbdddd9ae5
François de Metz <francois@2metz.fr>
2019-04-11 08:11:59 +0200

    Move marker to his own component.

--- js/app.vue
+++ js/app.vue
@@ -53,41 +53,12 @@
         <template
           v-for="category in markers"
         >
-          <MglMarker
+          <osm-marker
             v-for="marker in category.markers"
             :key="marker.id"
-            :coordinates="marker.geometry.coordinates"
-            anchor="bottom"
-          >
-            <div
-              slot="marker"
-            >
-              <svg
-                baseProfile="basic"
-                xmlns="http://www.w3.org/2000/svg"
-                width="48"
-                height="48"
-                viewBox="0 0 48 48"
-                class="marker-inside"
-              >
-                <path
-                  :fill="category.color"
-                  d="M24 0c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z" />
-                <foreignObject x="9" y="3" width="30" height="30">
-                  <div class="marker-icon">
-                    <v-icon :color="category.color">{{ category.icon }}</v-icon>
-                  </div>
-                </foreignObject>
-              </svg>
-            </div>
-            <MglPopup
-              :offset="72"
-              :close-button="false"
-              @added="(e) => e.popup.remove()"
-            >
-              <div>{{ marker.properties.name || category.name }}</div>
-            </MglPopup>
-          </MglMarker>
+            :marker="marker"
+            :category="category"
+          />
         </template>
         <mapillary-layer
           v-if="mapillaryLayer"
@@ -112,9 +83,10 @@
 </template>
 
 <script>
-import { MglMap, MglMarker, MglNavigationControl, MglPopup } from 'vue-mapbox/dist/vue-mapbox.umd.js';
+import { MglMap, MglNavigationControl } from 'vue-mapbox/dist/vue-mapbox.umd.js';
 import StyleControl from './style_control';
 import OsmSidebar from './sidebar';
+import OsmMarker from './marker';
 import MapillaryLayer from './mapillary_layer';
 import MapillaryControl from './mapillary_control';
 import MapillaryViewer from './mapillary_view';
@@ -123,11 +95,10 @@ import geojsondata from '../data/*.geojson';
 export default {
   components: {
     MglMap,
-    MglMarker,
-    MglPopup,
     MglNavigationControl,
     StyleControl,
     OsmSidebar,
+    OsmMarker,
     MapillaryLayer,
     MapillaryControl,
     MapillaryViewer
@@ -259,30 +230,6 @@ html {
 
 .mapillary-viewer {
   height: 100vh;
-}
-
-.mapboxgl-marker {
-  transition: none;
-}
-.marker-inside {
-  transition: transform .1s ease;
-  transform-origin: bottom center;
-  cursor: pointer;
-}
-.marker-inside:hover {
-  transform: scale(1.5);
-}
-.marker-icon {
-  background-color: white;
-  border-radius: 20px;
-  height: 30px;
-  width: 30px;
-}
-.marker-icon i {
-  height: 30px;
-  width: 30px;
-  padding: 5px;
-  font-size: 20px;
 }
 
 .mgl-map-wrapper {
---
+++ js/marker.vue
@@ -0,0 +1,81 @@
+<template>
+  <MglMarker
+    :coordinates="marker.geometry.coordinates"
+    anchor="bottom"
+  >
+    <div
+      slot="marker"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        class="marker-inside"
+      >
+        <path
+          :fill="category.color"
+          d="M24 0c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z"
+        />
+        <foreignObject x="9" y="3" width="30" height="30">
+          <div class="marker-icon">
+            <v-icon :color="category.color">{{ category.icon }}</v-icon>
+          </div>
+        </foreignObject>
+      </svg>
+    </div>
+    <MglPopup
+      :offset="72"
+      :close-button="false"
+      @added="(e) => e.popup.remove()"
+    >
+      <div>{{ marker.properties.name || category.name }}</div>
+    </MglPopup>
+  </MglMarker>
+</template>
+
+<script>
+import { MglMarker, MglPopup } from 'vue-mapbox/dist/vue-mapbox.umd.js';
+
+export default {
+  components: { MglMarker, MglPopup },
+
+  props: {
+    marker: {
+      type: Object,
+      required: true
+    },
+
+    category: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.mapboxgl-marker {
+  transition: none;
+}
+.marker-inside {
+  transition: transform .1s ease;
+  transform-origin: bottom center;
+  cursor: pointer;
+}
+.marker-inside:hover {
+  transform: scale(1.5);
+}
+.marker-icon {
+  background-color: white;
+  border-radius: 20px;
+  height: 30px;
+  width: 30px;
+}
+.marker-icon i {
+  height: 30px;
+  width: 30px;
+  padding: 5px;
+  font-size: 20px;
+}
+</style>
