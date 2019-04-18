024bfae69dab1fc434ec658dd0df850111a9a2b1
François de Metz <francois@2metz.fr>
2019-04-18 17:02:38 +0200

    Move some config to data.yml.

--- config.js
+++ config.js
@@ -1,20 +1,12 @@
-import { taxonomy } from './data.yml';
+import { config, taxonomy } from './data.yml';
 import imageSidebar from './images/montrouge.png';
 
-export const mapMaxBounds = [[2.293611, 48.807344], [2.336998, 48.825486]];
-export const mapCenter = [2.315111, 48.816614];
-export const mapZoom = 15;
-export const mapName = 'OSMontrouge';
-export const mapStyles = [
-  {
-    title: 'Bright',
-    uri: 'https://tiles.osmontrouge.fr/styles/bright/style.json',
-  },
-  {
-    title: 'Liberty',
-    uri: 'https://tiles.osmontrouge.fr/styles/liberty/style.json',
-  }
-];
+export const mapMaxBounds = config.map.maxBounds;
+export const mapCenter = config.map.center;
+export const mapZoom = config.map.zoom;
+export const mapName = config.name;
+export const mapStyles = config.map.styles;
+
 export const mapillaryUsers = [
   'e_ZBE6mFd7CYNjRSpLl-Lg', // francois2
   'C4ENdWpJdFNf8CvnQd7NrQ', // phyks
