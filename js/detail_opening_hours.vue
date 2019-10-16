3964898f6a8cf8c71f56b3af1b20f4ce61666982
François de Metz <francois@2metz.fr>
2019-10-17 00:05:41 +0200

    Display the next open/close event.

--- js/detail_opening_hours.vue
+++ js/detail_opening_hours.vue
@@ -1,11 +1,21 @@
 <template>
+  <v-list-item v-if="alwaysOpen">
+    <v-list-item-icon><v-icon>osm-time</v-icon></v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title>24/7</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
   <v-list-group
+    v-else
     prepend-icon="osm-time"
     no-action
   >
     <template v-slot:activator>
       <v-list-item-content>
-        <v-list-item-title>{{ $t(`details.opening_hours.${state}`) }}</v-list-item-title>
+        <v-list-item-title>
+          {{ $t(`details.opening_hours.${state}`) }}
+          {{ $t('details.opening_hours.until_date', { date: formatNextDate}) }}
+        </v-list-item-title>
       </v-list-item-content>
     </template>
 
@@ -44,6 +54,18 @@ export default {
         memo[day] = this.formatIntervals(this.openingHours.getOpenIntervals(...this.getDayOfWeek(index + 1)));
         return memo;
       }, {});
+    },
+    alwaysOpen() {
+      return this.nextChange ? false : true;
+    },
+    nextChange() {
+      return this.openingHours.getNextChange();
+    },
+    formatNextDate() {
+      const format = {
+        weekday: this.isToday(this.nextChange) || this.isTomorrow(this.nextChange) ? undefined : 'long'
+      };
+      return this.formatDate(this.nextChange, format);
     }
   },
 
@@ -57,13 +79,35 @@ export default {
       end.setHours(24, 0, 0, 0);
       return [start, end];
     },
+
     formatIntervals(intervals) {
       if (intervals.length === 0) {
         return this.$t(`details.opening_hours.false`);
       }
       return intervals.map((interval) => {
-        return interval.slice(0, 2).map(d => d.toLocaleString('fr', { hour: '2-digit', minute: '2-digit' })).join('-');
+        return interval.slice(0, 2).map(this.formatDate).join('-');
       }).join(' ');
+    },
+
+    formatDate(date, format={}) {
+      return date.toLocaleString('fr', { hour: '2-digit', minute: '2-digit', ...format });
+    },
+
+    isToday(date) {
+      const today = new Date()
+      return this.isSameDay(today, date);
+    },
+
+    isTomorrow(date) {
+      const tomorrow = new Date()
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      return this.isSameDay(tomorrow, date);
+    },
+
+    isSameDay(date1, date2) {
+      return date1.getFullYear() == date2.getFullYear() &&
+        date1.getMonth() == date2.getMonth() &&
+        date1.getDate() == date2.getDate();
     }
   }
 }
