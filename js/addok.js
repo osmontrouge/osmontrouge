ea925945e9627cd3b83f4f8a48138d63db7499e8
François de Metz <francois@2metz.fr>
2019-11-18 17:56:34 +0100

    Specify the type when doing a reverse search.

--- js/addok.js
+++ js/addok.js
@@ -1,5 +1,6 @@
-export function reverse(baseUrl, { lat, lon }) {
+export function reverse(baseUrl, { lat, lon, type = 'housenumber' }) {
   const url = new URL(`${baseUrl}/reverse`);
+  url.searchParams.append('type', type);
   url.searchParams.append('lat', lat);
   url.searchParams.append('lon', lon);
   return fetch(url).then(data => data.json());
